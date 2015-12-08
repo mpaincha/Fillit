8e9f2007aa8702ce860041994d3fb9032023c5ff
Maelle PAINCHAULT <mpaincha@e2r4p12.42.fr>
2015-12-08 15:34:36 +0100

    mise a jour

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: mpaincha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/07 14:31:47 by mpaincha          #+#    #+#             */
-/*   Updated: 2015/12/07 16:15:54 by mpaincha         ###   ########.fr       */
+/*   Updated: 2015/12/08 14:06:06 by mpaincha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,21 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-/*
-typedef	struct		s_elem
-{
-	void			*content;
-	int				nb_elem
-	struct s_list	*prev;
-	struct s_list	*next;
-}					t_elem;
 
-typedef struct		s_list
+typedef	struct			s_doublelist
 {
-	size_t			length;
-	struct s_elem	*p_tail;
-	struct s_elem	*p_head;
-}					t_list;
-*/
+	void				*content;
+	size_t				content_size;
+	size_t				nb_elem;
+	struct s_doublelist	*prev;
+	struct s_doublelist	*next;
+}						t_doublelist;
+
+int						ft_validite_piece(char *buf);
+int						ft_validite_char(char *buf);
+int						ft_validite_fichier(char *fichier);
+void					ft_lstdoubleadd(t_doublelist **alst, t_doublelist *new);
+t_doublelist			*ft_lstdoublenew(void const *content, size_t content_size, size_t nb_elem);
+void					ft_enregistrement(char *buf);
+
 #endif
