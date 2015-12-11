3982544fc56ac7b16a96bc1033bb9685649a23cd
Maelle PAINCHAULT <mpaincha@e2r5p12.42.fr>
2015-12-11 18:10:12 +0100

    debut travail resolution, non fonctionnel

--- ft_resolution.c
+++ ft_resolution.c
@@ -6,24 +6,45 @@
 /*   By: mpaincha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 11:37:37 by mpaincha          #+#    #+#             */
-/*   Updated: 2015/12/11 16:44:37 by mpaincha         ###   ########.fr       */
+/*   Updated: 2015/12/11 18:04:51 by mpaincha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+//verif si carre parfait strictement plus grand que 4 de cote :
 int		ft_verifdispo(char *carre, char	*piece, int cote, int pos)
 {
 	int		i;
+	int		hashtag;
 
 	i = 0;
-	while (piece[i])
+	hashtag = 0;
+	while (piece[i] && carre[pos])
 	{
-		if (piece[i] != '.' && carre[pos] == '.')
-			i++;
+		ft_putstr("deplacement piece i et carre pos \n"); //debug
+		ft_putstr("i : "); //debug
+		ft_putnbr(i); //debug
+		ft_putstr("\n pos : "); //debug
+		ft_putnbr(pos); //debug
+		ft_putstr(" \n"); //debug
+		if (piece[i] != '.')
+		{
+			if (carre[pos] == '.')
+			{
+				if (i == 3 || i == 7 || i == 11 || i == 18)
+					pos = pos + (cote - 4);				
+				else
+					pos++;
+				i++;
+				hashtag++;
+			}
+			else
+				return (0);
+		}
 		i++;
 	}
-	return (1);
+	return (hashtag == 4 ? 1 : 0);
 }
 
 int		ft_placement(t_dbllist *list_piece, char *carre, int cote)
@@ -34,12 +55,20 @@ int		ft_placement(t_dbllist *list_piece, char *carre, int cote)
 	pos = 0;
 	while (carre[pos])
 	{
+		ft_putstr("deplacement carre pos \n"); //debug
 		if (carre[pos] != '.')
+		{
+			ft_putstr("carre pos != ."); //debug
 			pos++;
+		}
 		else
 		{
+			ft_putstr("carre pos == . \n"); //debug
+			ft_putstr(list_piece->head->content);
 			if (ft_verifdispo(carre, list_piece->head->content, cote, pos))
 			{
+				ft_putstr("ici \n");
+				ft_putstr("dispo");
 			//	ft_putpiece(list_piece->head, cote);
 			//	if (ft_placement(list_piece->head->next))
 					return (1);
@@ -69,14 +98,6 @@ char	*ft_carrevide(int c)
 	return (carre);
 }
 
-/*
-int		ft_tailledbt(int nb_pieces)
-{
-	int		c;
-
-	return (c);
-}
-*/
 char	*ft_resolution(char *res, t_dbllist	*list_piece, int nb_pieces)
 {
 	int		cote;
@@ -87,7 +108,9 @@ char	*ft_resolution(char *res, t_dbllist	*list_piece, int nb_pieces)
 	ft_putstr(" \n"); //debug
 	ft_putstr("list recue : \n"); //debug
 	ft_putlsthead(list_piece); //debug
-	ft_placement(list_piece, ft_carrevide(cote), cote);
+	ft_putstr("\n"); //debug
+//	ft_placement(list_piece, ft_carrevide(cote), cote);
+	ft_placement(list_piece, "................", cote);
 	return (res);
 }
 
